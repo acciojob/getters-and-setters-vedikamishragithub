84776dbb3394b351481dc80d45889805665eb49e
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-07 22:32:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study() {
+    return this.name + ' is studying ';
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+    return this.name + ' is teaching ';
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
