b10e18316458417eadc752065120d2a40056bf05
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-09 12:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-//complete this code
 class Person {
   constructor(name, age) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
   }
-	get name(){
-		return this.name;
-	}
-	set age(){
-		return this.age;
-	}
+  get name(){
+    return this._name;
+  }
+  set age(newAge){
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
-	constructor(name, age){
-		super(name,age);
-		study() {
-         return this.name + ' is studying ';
-		
-	}
-	
+  constructor(name, age){
+    super(name,age);
+  }
+  study() {
+     return this._name + ' is studying ';
   }
 }
 
 class Teacher extends Person {
-	constructor(name, age){
-	 super(name,age);
-	 teach() {
-      return this.name + ' is teaching ';
-	}
-	
+  constructor(name, age){
+    super(name,age);
+  }
+  teach() {
+    return this._name + ' is teaching ';
   }
 }
+
 const person = new Person("John", 25);
 console.log(person.name); 
 
 person.age = 30;
-console.log(person.age);
+console.log(person._age);
 
 const student = new Student("Alice", 22);
-student.study();
+console.log(student.study());
 
 const teacher = new Teacher("Bob", 40);
-teacher.teach(); 
+console.log(teacher.teach()); 
+
 
 // Do not change the code below this line
 window.Person = Person;
